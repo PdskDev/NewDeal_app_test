74c58346e35d384879ac69945ecd537dd74e0667
PdskDev <prestadesk@gmail.com>
2022-07-21 23:40:09 +0200

    List Post page + bootstrap

---
+++ src/components/ListPosts.vue
@@ -0,0 +1,101 @@
+<template>
+  <div v-if="isLoading">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Spinner />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--If error occure-->
+  <div v-if="!loading && errorMessage">
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col">
+          <p class="h3 text-danger fw-bold">{{ errorMessage }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div v-if="!isLoading">
+      <div class="container mt-3" v-if="posts.length > 0">
+        <div class="row">
+          <div class="col">
+            <div class="row">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="post in posts" :key="post.id">
+                    <th scope="row">{{ post.id }}</th>
+                    <td>{{ post.title }}</td>
+                    <td>
+                      <router-link
+                        :to="`/posts/view/${post.id}`"
+                        class="btn btn-warning my-1"
+                        ><i class="fa fa-eye"></i
+                      ></router-link>
+                    </td>
+                    <td>
+                      <router-link
+                        :to="`/posts/edit/${post.id}`"
+                        class="btn btn-primary my-1"
+                        ><i class="fa fa-pen"></i
+                      ></router-link>
+                    </td>
+                    <td>
+                      <button
+                        class="btn btn-danger my-1"
+                        @click="submitDeleteContact(post.id)"
+                      >
+                        <i class="fa fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { PostServices } from '@/service/JsonServerService';
+import Spinner from '@/components/Spinner.vue';
+
+export default {
+  name: 'ListPosts',
+  data() {
+    return {
+      posts: [],
+      isLoading: false,
+      errorMessage: null,
+    };
+  },
+  created: async function () {
+    try {
+      this.isLoading = true;
+      let response = await PostServices.getAllPosts();
+      this.posts = response.data;
+      this.isLoading = false;
+    } catch (error) {
+      this.errorMessage = error;
+      this.isLoading = false;
+    }
+  },
+  components: { Spinner },
+};
+</script>
+<style scoped></style>
--- src/views/App.vue
+++ src/views/App.vue
@@ -1,46 +1,14 @@
 <template>
-  <div v-if="isLoading">
-    <Spinner />
-  </div>
-  <div>
-    <div class="container" v-if="!isLoading">
-      <div class="row">
-        <div class="col">
-          <div class="row" v-for="p in posts" :key="p.id">
-            <h5>{{ p.id }} - {{ p.title }}</h5>
-            <p>{{ p.body }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <NavBar />
+  <RouterView />
 </template>
 
 <script>
-import { JsonServerService } from '@/service/JsonServerService';
-import Spinner from '@/components/Spinner.vue';
-
+import NavBar from '@/components/NavBar';
 export default {
-  name: 'ListPosts',
-  data() {
-    return {
-      posts: [],
-      isLoading: false,
-      errorMessage: null,
-    };
-  },
-  created: async function () {
-    try {
-      this.isLoading = true;
-      let response = await JsonServerService.getAllPosts();
-      this.posts = response.data;
-      this.isLoading = false;
-    } catch (error) {
-      this.errorMessage = error;
-      this.isLoading = false;
-    }
-  },
-  components: { Spinner },
+  name: 'App',
+  components: { NavBar },
 };
 </script>
-<style scoped></style>
+
+<style></style>
